5acce0f60dc137983a92ae511ef5e6f7ad168fc5
danielmaica <danielmaica99@gmail.com>
2024-01-14 14:53:41 -0300

    alterando next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-  theme: {
-    extend: {
-      boxShadow: {
-        '3xl': 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
-      }
-    }
-  }
-}
+module.exports = nextConfig
